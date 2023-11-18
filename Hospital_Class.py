931694ce6d09fa25103bac5c823b4b148296809f
HoudaTouatii <151378505+HoudaTouatii@users.noreply.github.com>
2023-11-18 16:19:07 +0100

    Update Hospital_Class.py

--- Hospital_Class.py
+++ Hospital_Class.py
@@ -1,56 +1,28 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": []
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "execution_count": 20,
-      "metadata": {
-        "id": "Uo2gcRjakck6"
-      },
-      "outputs": [],
-      "source": [
-        "class Hospital:\n",
-        "  def __init__(self, name, list_of_departments):\n",
-        "    self.name = name\n",
-        "    self.list_of_departments = list_of_departments\n",
-        "\n",
-        "  def show_departments(self):\n",
-        "    print(\"These are the available departments:\")\n",
-        "    for dep in self.list_of_departments:\n",
-        "      print(dep.name)\n",
-        "    print(\"============================\")\n",
-        "\n",
-        "  def add_department(self, new_dep_name):\n",
-        "    new_dep_instance = Department(new_dep_name, 0)\n",
-        "    self.list_of_departments.append(new_dep_instance)\n",
-        "\n",
-        "\n",
-        "  def remove_department (self, old_department):\n",
-        "    for dep in self.list_of_departments:\n",
-        "      if old_department == dep.name:\n",
-        "        self.list_of_departments.remove(dep)\n",
-        "\n",
-        "\n",
-        "  def find_department_by_name(self):\n",
-        "    self.show_departments()\n",
-        "    user_dep = str(input('choose your department:'))\n",
-        "    for dep in self.list_of_departments:\n",
-        "      if dep.name == user_dep:\n",
-        "        return dep"
-      ]
-    }
-  ]
-}
+class Hospital:
+  def __init__(self, name, list_of_departments):
+    self.name = name
+    self.list_of_departments = list_of_departments
+    
+  def show_departments(self):
+    print("These are the available departments:")
+    for dep in self.list_of_departments:
+      print(dep.name)
+    print("============================")
+
+  def add_department(self, new_dep_name):
+    new_dep_instance = Department(new_dep_name, 0)
+    self.list_of_departments.append(new_dep_instance)
+
+
+  def remove_department (self, old_department):
+    for dep in self.list_of_departments:
+      if old_department == dep.name:
+        self.list_of_departments.remove(dep)
+
+
+  def find_department_by_name(self):
+    self.show_departments()
+    user_dep = str(input('choose your department:'))
+    for dep in self.list_of_departments:
+      if dep.name == user_dep:
+        return dep
